e7d959d904a8b20ccfe44369a582a714be933147
YingJie Zhu <zhuyingjie1230@qq.com>
2023-03-07 10:35:03 +0800

    提交“课程计划”的代码

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,7 +1,11 @@
 package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -10,6 +14,16 @@ import com.xuecheng.content.model.po.Teachplan;
  *
  * @author itcast
  */
+@Repository
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+    /**
+     * @description 查询某课程的课程计划，组成树型结构
+     * @param courseId
+     * @return com.xuecheng.content.model.dto.TeachplanDto
+     * @author Mr.M
+     * @date 2022/9/9 11:10
+     */
+    List<TeachplanDto> selectTreeNodes(long courseId);
 
 }
